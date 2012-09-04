58f144cb156d07cf94361e113479e83d7b85867e
arun ganuga <arun.srivatsav@gmail.com>
2012-09-04 17:58:44 +0530

    changes to log the stack trace to a file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,13 @@ class ApplicationController < ActionController::Base
 
   def rescue_action_in_public(exception)
     flash[:notice] = I18n.t(translate_string_for(exception))
+    exception_file = "#{RAILS_ROOT}/exception_log.txt"
+    File.open(exception_file, 'a') {|f| f.write(exception.message + "\n" + exception.backtrace.join("\n")) }
     redirect_to posts_path
   end
 
   def translate_string_for(exception)
     t_string = exception.class.to_s.underscore.gsub(%r|/|, '.')
-    return "exception_messages.#{t_string}";
+    return "exception_messages.#{t_string}"
   end
 end
