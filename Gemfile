9be435613f30aee13884728783f4a84c118478ff
kishore <kishore@kishore-pc.(none)>
2012-09-04 18:36:39 +0530

    removed i18n gem, as the functionality is already available in rails

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source :gemcutter
 
 gem 'rails', "~> 2.3.12"
-gem 'i18n'
 gem 'sqlite3'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  I18n.load_path = ['en.yml']
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
